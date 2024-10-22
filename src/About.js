0b0ec6010f3cec90e4480764002ac28160ceaca4
Ragulraja47 <cragulraja2004@gmail.com>
2024-10-22 09:37:19 +0530

    second commit

--- src/About.js
+++ src/About.js
@@ -10,7 +10,7 @@ function About(){
                 <div className='w-1/2 flex justify-center'>
                     <div className='flex flex-col justify-center'>
                     <h1 className='text-4xl border-b-4 border-color mb-5 w-[170px] font-bold'>About me</h1>
-                    <p className=''>Aspiring Full Stack developer with strong Foundation in Java Language</p>
+                    <p className=''>Aspiring Full Stack developer with strong Foundation in Java Language and eager to develop user friendly websites</p>
                     </div>
                 </div>
             </section>
